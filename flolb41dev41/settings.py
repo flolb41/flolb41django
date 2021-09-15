90dd0efffb2367b1a4b607652a0ed1413f49e33f
Flolb41 <59803497+flolb41@users.noreply.github.com>
2021-09-15 21:51:42 +0200

    passage en prod v1

--- flolb41dev41/settings.py
+++ flolb41dev41/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(8sgckn6=*=%%302!ofdc&y7=f6g57tr#&a0zd*zbe^v1t1rpa'
+SECRET_KEY = '(8sgckn6=*=%%302!ofdc&y7=f6g57tr#&a0zd*zbe^v1t1rpa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["flolb41-dev41.com"]
 
 
 # Application definition
@@ -137,3 +137,15 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_HSTS_SECONDS = 60
+
+SECURE_SSL_REDIRECT = True
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
+
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+SECURE_HSTS_PRELOAD = True
